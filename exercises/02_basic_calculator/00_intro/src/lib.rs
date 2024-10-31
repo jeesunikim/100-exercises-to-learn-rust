8a46c136dea21f53597b523dda4935c6eb8eb343
Jeesun Kim <3912060+jeesunikim@users.noreply.github.com>
2024-10-31 15:47:48 -0700

    the first two exercises completed

--- exercises/02_basic_calculator/00_intro/src/lib.rs
+++ exercises/02_basic_calculator/00_intro/src/lib.rs
@@ -1,3 +1,6 @@
+// & indicates that the function returns a reference
+// 'static is a lifetime specifier that tells Rust this reference will live for the entire duration of the program
+// str is the type of data being referred to (a string slice)
 fn intro() -> &'static str {
     // TODO: fix me 👇
     "I'm ready to __!"
